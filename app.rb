6143fc47a4e4f0694345c572737d5a6686a9f588
erik-paulstrom <55193928+erik-paulstrom@users.noreply.github.com>
2021-02-05 11:18:07 +0100

    commit

--- app.rb
+++ app.rb
@@ -8,3 +8,18 @@ enable :sessions
 get('/') do 
     slim(:register)
 end 
+
+post('/users/new') do 
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    if (password == password_confirm)
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new('db/guitar_webshop.db')
+        db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+        redirect('/')
+    else
+        "Skriv ett lösenord som matchar de angivna."
+    end
+
+end 
